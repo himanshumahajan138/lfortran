99ab4b64ac50737cc252dd88102dfa88aa3d864b
himanshumahajan138 <himanshumahajan138@gmail.com>
2026-09-07 23:57:41 +0530

    test: move nopass regression test to integration tests

--- tests/class_procedure_nopass_1.f90
+++
@@ -1,8 +0,0 @@
-program class_procedure_nopass_1
-! A type bound procedure declared outside of any module, so the symbol
-! table visitor must not consult an enclosing module symbol here.
-type :: mt
-contains
-    procedure, nopass :: g
-end type
-end program
